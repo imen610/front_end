95e7cf8b0d63104c841dfd17d81fae3b87e32d44
imen <imenmjadri44@gmail.com>
2022-04-24 15:57:46 +0200

    implementation de la connexion entre l'api et la partie frontend

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,110 +1,145 @@
-
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:mon_projet/pages/regi_page.dart';
 import 'package:mon_projet/widgets/btn_widget.dart';
-
+// ignore: import_of_legacy_library_into_null_safe
+import 'package:http/http.dart' as http;
+import '../second.dart';
 import '../utils/color.dart';
+import 'dart:convert';
 import '../widgets/herder_container.dart';
-
-
 
 class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
-
 
   @override
   State<LoginPage> createState() => _LoginPageState();
 }
 
 class _LoginPageState extends State<LoginPage> {
+  var passController = TextEditingController();
+  var emailController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        padding: EdgeInsets.only(bottom: 30),
+        padding: const EdgeInsets.only(bottom: 30),
         child: Column(
           children: <Widget>[
             HeaderContainer("Login"),
             Expanded(
               flex: 1,
-
-            child : Container(
-              margin: EdgeInsets.only(left:20,right:20,top:30),
-              child: Column(
-                mainAxisSize: MainAxisSize.max,
-                children: <Widget>[
-                  _textInput(hint:"Email",icon:Icons.email),
-                  _textInput(hint:"password",icon:Icons.vpn_key),
-                  Container(
-                    margin: EdgeInsets.only(top:10),
-                    alignment: Alignment.centerRight,
-                    child: Text(
-                      "Forgot Password?"
+              child: Container(
+                margin: const EdgeInsets.only(left: 20, right: 20, top: 30),
+                child: Column(
+                  mainAxisSize: MainAxisSize.max,
+                  children: [
+                    TextFormField(
+                      controller: emailController,
+                      // ignore: prefer_const_constructors
+                      decoration: InputDecoration(
+                          labelText: "Email",
+                          border: const OutlineInputBorder(),
+                          suffixIcon: const Icon(Icons.email)),
                     ),
-                  ),
-                  Expanded(
-                    child: Center(
-                      child: ButtonWidget(
-                        onClick: () {
-                          Navigator.push(
-                            context, MaterialPageRoute(
-                            builder : (context) => RegPage()
-                          )
-                          );
-                        },
-                        btnText: "LOGIN",
+                    const SizedBox(height: 15),
+                    TextFormField(
+                      controller: passController,
+                      obscureText: true,
+                      // ignore: prefer_const_constructors
+                      decoration: InputDecoration(
+                          labelText: "Password",
+                          border: const OutlineInputBorder(),
+                          suffixIcon: const Icon(Icons.vpn_key)),
+                    ),
+                    // _textInput(hint:"Email",icon:Icons.email),
+                    // _textInput(hint:"password",icon:Icons.vpn_key),
+                    Container(
+                      margin: const EdgeInsets.only(top: 10),
+                      alignment: Alignment.centerRight,
+                      child: const Text("Forgot Password?"),
+                    ),
+                    Expanded(
+                      child: Center(
+                        child: ButtonWidget(
+                          onClick: () {
+                            login();
+                          },
+                          btnText: "LOGIN",
+                        ),
                       ),
                     ),
-                  ),
-                  RichText(
-                      text:TextSpan(
+                    // OutlinedButton.icon(
+                    //     onPressed: () {
+                    //       login();
+                    //     },
+                    //     icon: const Icon(Icons.login, size: 18),
+                    //     label: const Text("Login")),
+                    RichText(
+                      text: TextSpan(
                         children: [
-                          TextSpan(text:"Don't have an account ?", style:TextStyle(color : Colors.black)),
-                          TextSpan(text:"Registor",recognizer: new TapGestureRecognizer()..onTap = () => {
-                          Navigator.push(
-                          context, MaterialPageRoute(
-                          builder : (context) => RegPage()
-                          )
-                          )
-                          }, style:TextStyle(color : orangeColors)),
-
+                          const TextSpan(
+                              text: "Don't have an account ?",
+                              style: TextStyle(color: Colors.black)),
+                          TextSpan(
+                              text: "Registor",
+                              recognizer: new TapGestureRecognizer()
+                                ..onTap = () => {
+                                      Navigator.push(
+                                          context,
+                                          MaterialPageRoute(
+                                              builder: (context) => RegPage()))
+                                    },
+                              style: TextStyle(color: orangeColors)),
                         ],
-
-
                       ),
-                  ),
-              ],
+                    ),
+                  ],
+                ),
+              ),
             ),
-            ),
-            ),
-            
           ],
         ),
-
       ),
-
     );
   }
 
-  Widget _textInput({controller, hint, icon}){
-    return(Container(
-      margin: EdgeInsets.only(top:10),
-      decoration:BoxDecoration(
+  Widget _textInput({controller, hint, icon}) {
+    return (Container(
+      margin: EdgeInsets.only(top: 10),
+      decoration: BoxDecoration(
         borderRadius: BorderRadius.all(Radius.circular(20)),
         color: Colors.white,
-
       ),
       padding: EdgeInsets.only(left: 10),
       child: TextFormField(
         decoration: InputDecoration(
-            border : InputBorder.none,
-            hintText: hint,
-            prefixIcon: Icon(icon)
-        ),
-
+            border: InputBorder.none, hintText: hint, prefixIcon: Icon(icon)),
       ),
-    )
-    );
+    ));
+  }
+
+  // create function to call login API
+  Future<void> login() async {
+    if (passController.text.isNotEmpty && emailController.text.isNotEmpty) {
+      var response = await http.post(
+          Uri.parse("http://localhost:8000/auth/login/"),
+          headers: {"Content-Type": "application/json"},
+          body: (jsonEncode({
+            "email": emailController.text,
+            "password": passController.text
+          })));
+      print(response.statusCode);
+      if (response.statusCode != 200) {
+        ScaffoldMessenger.of(context)
+            .showSnackBar(const SnackBar(content: Text("invalid")));
+      } else {
+        Navigator.push(
+            context, MaterialPageRoute(builder: (context) => Second()));
+      }
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(content: Text("black field not allowed")));
+    }
   }
 }
--- lib/pages/regi_page.dart
+++ lib/pages/regi_page.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:mon_projet/widgets/btn_widget.dart';
-
+import 'package:http/http.dart';
+import 'dart:convert';
+import '../second.dart';
 import '../utils/color.dart';
 import '../widgets/herder_container.dart';
+
 class RegPage extends StatefulWidget {
   const RegPage({Key? key}) : super(key: key);
 
@@ -12,6 +15,33 @@ class RegPage extends StatefulWidget {
 }
 
 class _RegPageState extends State<RegPage> {
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+  TextEditingController usernameController = TextEditingController();
+
+  void sigup(String email, username, password) async {
+    try {
+      Response response = await post(
+          Uri.parse('http://127.0.0.1:8000/auth/register/'),
+          body: {'email': email, 'username': username, 'password': password});
+
+      if (response.statusCode == 200) {
+        var data = jsonDecode(response.body.toString());
+        //print(data['token']);
+        // Navigator.push(
+        //     context, MaterialPageRoute(builder: (context) => Second()));
+
+        print('account created successfully');
+      } else {
+        Navigator.push(
+            context, MaterialPageRoute(builder: (context) => Second()));
+        print('failed');
+      }
+    } catch (e) {
+      print(e.toString());
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,66 +52,90 @@ class _RegPageState extends State<RegPage> {
             HeaderContainer("register"),
             Expanded(
               flex: 1,
-
-              child : Container(
-                margin: EdgeInsets.only(left:20,right:20,top:30),
+              child: Container(
+                margin: EdgeInsets.only(left: 20, right: 20, top: 30),
                 child: Column(
-                  mainAxisSize: MainAxisSize.max,
-                  children: <Widget>[
-                    _textInput(hint:"Username",icon:Icons.person),
-                    _textInput(hint:"Email",icon:Icons.email),
-                    _textInput(hint:"Password",icon:Icons.vpn_key),
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    TextFormField(
+                      controller: emailController,
+                      decoration: InputDecoration(
+                          border: InputBorder.none, hintText: "Email"),
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    TextFormField(
+                      controller: usernameController,
+                      decoration: InputDecoration(
+                          border: InputBorder.none, hintText: "username"),
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    TextFormField(
+                      controller: passwordController,
+                      decoration: InputDecoration(
+                          border: InputBorder.none, hintText: "password"),
+                    ),
+                    // child: Column(
+                    //   mainAxisSize: MainAxisSize.max,
+                    //   children: <Widget>[
+                    //     _textInput(hint:"Username",icon:Icons.person),
+                    //     _textInput(hint:"Email",icon:Icons.email),
+                    //     _textInput(hint:"Password",icon:Icons.vpn_key),
 
                     Expanded(
                       child: Center(
-                        child: ButtonWidget(btnText: "REGISTER",
-                        onClick: () {
-                          Navigator.pop(context);
-                        },),
-
+                        child: ButtonWidget(
+                          btnText: "REGISTER",
+                          onClick: () {
+                            sigup(
+                                emailController.text.toString(),
+                                usernameController.text.toString(),
+                                passwordController.text.toString());
+                          },
+                        ),
                       ),
                     ),
                     RichText(
-                      text:TextSpan(
+                      text: TextSpan(
                         children: [
-                          TextSpan(text:"Already a member ?", style:TextStyle(color : Colors.black),),
-                          TextSpan(text:"Login", recognizer: new TapGestureRecognizer()..onTap = () => {
-                          Navigator.pop(context)
-                          },style:TextStyle(color : orangeColors)),
+                          TextSpan(
+                            text: "Already a member ?",
+                            style: TextStyle(color: Colors.black),
+                          ),
+                          TextSpan(
+                              text: "Login",
+                              recognizer: new TapGestureRecognizer()
+                                ..onTap = () => {Navigator.pop(context)},
+                              style: TextStyle(color: orangeColors)),
                         ],
-
                       ),
                     ),
                   ],
                 ),
               ),
             ),
-
           ],
         ),
-
       ),
-
     );
   }
-  Widget _textInput({controller, hint, icon}){
-    return(Container(
-      margin: EdgeInsets.only(top:10),
-      decoration:BoxDecoration(
+
+  Widget _textInput({controller, hint, icon}) {
+    return (Container(
+      margin: EdgeInsets.only(top: 10),
+      decoration: BoxDecoration(
         borderRadius: BorderRadius.all(Radius.circular(20)),
         color: Colors.white,
-
       ),
       padding: EdgeInsets.only(left: 10),
       child: TextFormField(
         decoration: InputDecoration(
-            border : InputBorder.none,
-            hintText: hint,
-            prefixIcon: Icon(icon)
-        ),
-
+            border: InputBorder.none, hintText: hint, prefixIcon: Icon(icon)),
       ),
-    )
-    );
+    ));
   }
 }
